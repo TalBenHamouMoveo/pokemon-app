0da6b52c30e074b59c230cc82fd0c8d9c9b14670
TalBenHamouMoveo <talb@moveo.co.il>
2024-03-04 11:01:32 +0200

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-list/pokemon-detail/pokemon-detail.component';
+import { PokemonListItemComponent } from './pokemon-list/pokemon-list-item/pokemon-list-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    PokemonDetailComponent,
+    PokemonListItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
