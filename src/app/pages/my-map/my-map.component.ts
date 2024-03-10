3abf63db89cbab2fc8186e9c705d2bda1755fa74
TalBenHamouMoveo <talb@moveo.co.il>
2024-03-10 11:11:22 +0200

    notes done except the .env file.

--- src/app/pages/my-map/my-map.component.ts
+++ src/app/pages/my-map/my-map.component.ts
@@ -1,7 +1,5 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { Loader } from '@googlemaps/js-api-loader';
-
-// const apiKey: string = process.env.GMaps_API_Key;
+import { GooglemapService } from '../../service/googlemap.service';
 
 @Component({
   selector: 'app-my-map',
@@ -9,56 +7,15 @@ import { Loader } from '@googlemaps/js-api-loader';
   styleUrls: ['./my-map.component.scss']
 })
 export class MyMapComponent implements AfterViewInit {
-  marker: google.maps.Marker;
-  map: google.maps.Map;
-  lat = 32.06244076;
-  lng = 34.7707653;
-  coordinates: google.maps.LatLng;
-  mapOptions: google.maps.MapOptions;
   @ViewChild('mapContainer', { static: false }) gmap: ElementRef<HTMLInputElement>;
 
+  constructor(private googleMapService: GooglemapService) {}
+
   ngAfterViewInit(): void {
-    const loader = new Loader({ apiKey: "AIzaSyBnChFKtfer3IJQndl1miHj1C_BSlX9Dpw" }).load().then(() => {
-      this.coordinates = new google.maps.LatLng(this.lat, this.lng);
-      this.mapOptions = {
-        center: this.coordinates,
-        zoom: 15,
-      };
-      this.marker = new google.maps.Marker({
-        position: this.coordinates,
-      });
-      this.mapInitializer();
-      this.initAutocomplete();
-    });
-  }
-
-  mapInitializer() {
-    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
-    this.marker.setMap(this.map);
-  }
-
-  initAutocomplete() {
-    const input = document.getElementById('autocomplete') as HTMLInputElement;
-    const autocomplete = new google.maps.places.Autocomplete(input);
-    this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
-    autocomplete.bindTo('bounds', this.map);
-    autocomplete.setFields(['address_components', 'geometry', 'name']);
-
-    autocomplete.addListener('place_changed', () => {
-      const place = autocomplete.getPlace();
-      if (!place.geometry || !place.geometry.location) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      const bounds = new google.maps.LatLngBounds();
-      this.marker.setPosition(place.geometry.location);
-
-      if (place.geometry.viewport) {
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-      this.map.fitBounds(bounds);
+    this.googleMapService.loadMapAPI().then(() => {
+      this.googleMapService.initializeMap(this.gmap.nativeElement);
+      const input = document.getElementById('autocomplete') as HTMLInputElement;
+      this.googleMapService.initAutocomplete(input);
     });
   }
 }
--- src/app/service/googlemap.service.ts
+++ src/app/service/googlemap.service.ts
@@ -1,13 +1,61 @@
 import { Injectable } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
+import { Coordinates } from '../constants/coordinates';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GooglemapService {
+  private loader: Loader;
+  private map: google.maps.Map;
+  private marker: google.maps.Marker;
+  private office: Coordinates = {
+    lat: 32.06244076,
+    lng: 34.7707653
+  };
 
+  constructor() {
+    this.loader = new Loader({ apiKey: "AIzaSyBnChFKtfer3IJQndl1miHj1C_BSlX9Dpw" });
+  }
 
-  constructor() { }
+  loadMapAPI(): Promise<typeof google> {
+    return this.loader.load();
+  }
 
-  getLanLet() {
+  initializeMap(element: HTMLElement): void {
+    const coordinates = new google.maps.LatLng(this.office.lat, this.office.lng);
+    const mapOptions: google.maps.MapOptions = {
+      center: coordinates,
+      zoom: 15,
+    };
+    this.map = new google.maps.Map(element, mapOptions);
+    this.marker = new google.maps.Marker({
+      position: coordinates,
+      map: this.map
+    });
+  }
+
+  initAutocomplete(input: HTMLInputElement): void {
+    const autocomplete = new google.maps.places.Autocomplete(input);
+    this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+    autocomplete.bindTo('bounds', this.map);
+    autocomplete.setFields(['address_components', 'geometry', 'name']);
+
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      const bounds = new google.maps.LatLngBounds();
+      this.marker.setPosition(place.geometry.location);
+
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+      this.map.fitBounds(bounds);
+    });
   }
 }
